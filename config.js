70f2683a2b7f57eb3568a88e4a1861f982f0f610
ClemDuss <56772098+ClemDuss@users.noreply.github.com>
2021-11-17 22:26:39 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+    applications: [
+        {
+            image: './images/dotelec.PNG',
+            nom: 'DOTELEC V9',
+            url: 'http://localhost:8081/DotelecWeb'
+        },
+        {
+            image: './images/onlyoffice.jpeg',
+            nom: 'Example OnlyOffice',
+            url: 'http://localhost/example'
+        }
+    ]
+}
